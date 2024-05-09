ebbe7ae36c822e94fc4bca0dc8393407d52cacc4
Hibi_10000 <87810571+Hibi-10000@users.noreply.github.com>
2024-05-09 22:03:44 +0900

    chore: Gradle Kotlin DSLに変更

---
+++ build.gradle.kts
@@ -0,0 +1,73 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    java
+    kotlin("jvm") version "1.9.24"
+	id("fabric-loom") version "1.6-SNAPSHOT"
+}
+
+version = project.properties["mod_version"] as String
+group = project.properties["maven_group"] as String
+
+java.sourceCompatibility = JavaVersion.VERSION_17
+java.targetCompatibility = JavaVersion.VERSION_17
+kotlin.compilerOptions.jvmTarget.set(JvmTarget.JVM_17)
+
+base.archivesName.set(project.properties["archives_base_name"] as String)
+
+repositories {
+	// Add repositories to retrieve artifacts from in here.
+	// You should only use this when depending on other mods because
+	// Loom adds the essential maven repositories to download Minecraft and libraries from automatically.
+	// See https://docs.gradle.org/current/userguide/declaring_repositories.html
+	// for more information about repositories.
+}
+
+loom {
+    splitEnvironmentSourceSets()
+
+	mods {
+        create("plategate") {
+			sourceSet(sourceSets["main"])
+			sourceSet(sourceSets["client"])
+		}
+	}
+}
+
+dependencies {
+	// To change the versions see the gradle.properties file
+	minecraft("com.mojang:minecraft:${project.properties["minecraft_version"]}")
+	mappings("net.fabricmc:yarn:${project.properties["yarn_mappings"]}:v2")
+	modImplementation("net.fabricmc:fabric-loader:${project.properties["loader_version"]}")
+
+	// Fabric API. This is technically optional, but you probably want it anyway.
+	modImplementation("net.fabricmc.fabric-api:fabric-api:${project.properties["fabric_version"]}")
+	modImplementation("net.fabricmc:fabric-language-kotlin:${project.properties["fabric_kotlin_version"]}")
+}
+
+tasks {
+    processResources {
+        inputs.property("version", project.version)
+
+        filesMatching("fabric.mod.json") {
+            expand("version" to project.version)
+        }
+    }
+
+    withType<JavaCompile> {
+        options.release.set(17)
+    }
+
+    jar {
+        from("LICENSE.txt") {
+            rename { "${it}_${project.base.archivesName.get()}"}
+        }
+    }
+}
+
+java {
+	// Loom will automatically attach sourcesJar to a RemapSourcesJar task and to the "build" task
+	// if it is present.
+	// If you remove this line, sources will not be generated.
+	withSourcesJar()
+}
