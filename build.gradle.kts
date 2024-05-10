ca7be15e4027be90fcc6fdf5f21a2b150efaa78c
Hibi_10000 <87810571+Hibi-10000@users.noreply.github.com>
2024-05-10 13:12:55 +0900

    chore: インデント修正等

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     java
     kotlin("jvm") version "1.9.24"
-	id("fabric-loom") version "1.6-SNAPSHOT"
+    id("fabric-loom") version "1.6-SNAPSHOT"
 }
 
 version = project.properties["mod_version"] as String
@@ -16,33 +16,33 @@ kotlin.compilerOptions.jvmTarget.set(JvmTarget.JVM_17)
 base.archivesName.set(project.properties["archives_base_name"] as String)
 
 repositories {
-	// Add repositories to retrieve artifacts from in here.
-	// You should only use this when depending on other mods because
-	// Loom adds the essential maven repositories to download Minecraft and libraries from automatically.
-	// See https://docs.gradle.org/current/userguide/declaring_repositories.html
-	// for more information about repositories.
+    // Add repositories to retrieve artifacts from in here.
+    // You should only use this when depending on other mods because
+    // Loom adds the essential maven repositories to download Minecraft and libraries from automatically.
+    // See https://docs.gradle.org/current/userguide/declaring_repositories.html
+    // for more information about repositories.
 }
 
 loom {
     splitEnvironmentSourceSets()
 
-	mods {
+    mods {
         create("plategate") {
-			sourceSet(sourceSets["main"])
-			sourceSet(sourceSets["client"])
-		}
-	}
+            sourceSet(sourceSets["main"])
+            sourceSet(sourceSets["client"])
+        }
+    }
 }
 
 dependencies {
-	// To change the versions see the gradle.properties file
-	minecraft("com.mojang:minecraft:${project.properties["minecraft_version"]}")
-	mappings("net.fabricmc:yarn:${project.properties["yarn_mappings"]}:v2")
-	modImplementation("net.fabricmc:fabric-loader:${project.properties["loader_version"]}")
+    // To change the versions see the gradle.properties file
+    minecraft("com.mojang:minecraft:${project.properties["minecraft_version"]}")
+    mappings("net.fabricmc:yarn:${project.properties["yarn_mappings"]}:v2")
+    modImplementation("net.fabricmc:fabric-loader:${project.properties["loader_version"]}")
 
-	// Fabric API. This is technically optional, but you probably want it anyway.
-	modImplementation("net.fabricmc.fabric-api:fabric-api:${project.properties["fabric_version"]}")
-	modImplementation("net.fabricmc:fabric-language-kotlin:${project.properties["fabric_kotlin_version"]}")
+    // Fabric API. This is technically optional, but you probably want it anyway.
+    modImplementation("net.fabricmc.fabric-api:fabric-api:${project.properties["fabric_version"]}")
+    modImplementation("net.fabricmc:fabric-language-kotlin:${project.properties["fabric_kotlin_version"]}")
 }
 
 tasks {
@@ -60,14 +60,14 @@ tasks {
 
     jar {
         from("LICENSE.txt") {
-            rename { "${it}_${project.base.archivesName.get()}"}
+            rename { "${it}_${project.base.archivesName.get()}" }
         }
     }
 }
 
 java {
-	// Loom will automatically attach sourcesJar to a RemapSourcesJar task and to the "build" task
-	// if it is present.
-	// If you remove this line, sources will not be generated.
-	withSourcesJar()
+    // Loom will automatically attach sourcesJar to a RemapSourcesJar task and to the "build" task
+    // if it is present.
+    // If you remove this line, sources will not be generated.
+    withSourcesJar()
 }
--- src/client/kotlin/com/github/hibi_10000/mods/fabric/PlateGateClient.kt
+++ src/client/kotlin/com/github/hibi_10000/mods/fabric/PlateGateClient.kt
@@ -3,7 +3,7 @@ package com.github.hibi_10000.mods.fabric
 import net.fabricmc.api.ClientModInitializer
 
 object PlateGateClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
+    override fun onInitializeClient() {
+        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+    }
 }
