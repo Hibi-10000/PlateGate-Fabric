34ec155bea52b4cb91f23712e767e391c379307b
Hibi_10000 <87810571+Hibi-10000@users.noreply.github.com>
2024-06-16 11:28:40 +0900

    refactor: 不要そうなものを削除

--- src/main/kotlin/com/github/hibi_10000/mods/fabric/commands/plategate/PGCreate.kt
+++ src/main/kotlin/com/github/hibi_10000/mods/fabric/commands/plategate/PGCreate.kt
@@ -7,9 +7,6 @@ package com.github.hibi_10000.mods.fabric.commands.plategate
 import com.mojang.brigadier.Command
 import com.mojang.brigadier.arguments.StringArgumentType
 import com.mojang.brigadier.context.CommandContext
-import net.minecraft.command.argument.EntityArgumentType
-import net.minecraft.command.argument.RotationArgumentType
-import net.minecraft.command.argument.Vec3ArgumentType
 import net.minecraft.server.command.CommandManager.argument
 import net.minecraft.server.command.CommandManager.literal
 import net.minecraft.server.command.ServerCommandSource
@@ -17,37 +14,14 @@ import net.minecraft.text.Text
 
 object PGCreate {
     internal val CommandLiteral = literal("create").then(
-        argument("name", StringArgumentType.string()).then(
-            argument("destination", EntityArgumentType.entity()).executes { context ->
-                val source = context.source
-                val sourceP = source.playerOrThrow
-                val name = StringArgumentType.getString(context, "name")
-                val destination = EntityArgumentType.getEntity(context, "destination")
-                execute(context)
-            }
-        ).then(
-            argument("location", Vec3ArgumentType.vec3()).then(
-                //TODO: northとかで指定?
-                argument("rotation", RotationArgumentType.rotation()).executes { context ->
-                    val source = context.source
-                    val sourceP = source.playerOrThrow
-                    val name = StringArgumentType.getString(context, "name")
-                    val location = Vec3ArgumentType.getVec3(context, "location")
-                    val rotation = RotationArgumentType.getRotation(context, "rotation")
-                    execute(context)
-                }
-            )
-        ).executes { context ->
-            val source = context.source
-            val sourceP = source.playerOrThrow
-            val name = StringArgumentType.getString(context, "name")
-            execute(context)
-        }
+        argument("name", StringArgumentType.string()).executes(this::execute)
     )
 
     private fun execute(context: CommandContext<ServerCommandSource>): Int {
-        println("PGCreate executed ${context.input}")
-        context.source.sendFeedback({ Text.literal("Called /plategate create ${context.input}") }, true)
+        val source = context.source
+        val sourceP = source.playerOrThrow
+        val name = StringArgumentType.getString(context, "name")
+        context.source.sendFeedback({ Text.literal("Called /plategate create $name") }, true)
         return Command.SINGLE_SUCCESS
     }
 }
