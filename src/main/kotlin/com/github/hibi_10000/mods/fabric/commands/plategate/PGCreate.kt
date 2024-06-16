09acaa7bc85b128bb12ad6ae9b26ee0c3c9001eb
Hibi_10000 <87810571+Hibi-10000@users.noreply.github.com>
2024-06-16 18:11:05 +0900

    feat: redirect前のコマンドを取得できるように

--- src/main/kotlin/com/github/hibi_10000/mods/fabric/commands/plategate/PGCreate.kt
+++ src/main/kotlin/com/github/hibi_10000/mods/fabric/commands/plategate/PGCreate.kt
@@ -4,6 +4,7 @@
 
 package com.github.hibi_10000.mods.fabric.commands.plategate
 
+import com.github.hibi_10000.mods.fabric.LiteralCommandNodeAlias
 import com.mojang.brigadier.Command
 import com.mojang.brigadier.arguments.StringArgumentType
 import com.mojang.brigadier.context.CommandContext
@@ -21,7 +22,8 @@ object PGCreate {
         val source = context.source
         val sourceP = source.playerOrThrow
         val name = StringArgumentType.getString(context, "name")
-        context.source.sendFeedback({ Text.literal("Called /plategate create $name") }, true)
+        val alias = (context.rootNode as LiteralCommandNodeAlias).`plateGate$getAlias`()
+        context.source.sendFeedback({ Text.literal("Called /$alias create $name") }, true)
         return Command.SINGLE_SUCCESS
     }
 }
